f76f3c9082744dade8f408e392179ac358287496
Alessandro Cioffi <alessandrocioffi007@gmail.com>
2025-03-19 19:15:20 +0100

    Update esempio.c

--- Memoria_Condivisa/esempio.c
+++ Memoria_Condivisa/esempio.c
@@ -1,22 +1,26 @@
-int main(){
+#include <sys/shm.h> 
+#include <sys/ipc.h>  
+#include <stdio.h> 
+#include <stdlib.h> 
 
-        key_t shm_key = IPC_PRIVATE;
- 	int ds_shm = shmget(shm_key, sizeof(int),IPC_CREAT|0664);
- 	
- 	if(ds_shm < 0) { 
- 		perror("errore shmget!");
- 		exit(1); 
- 	}
+int main() {
+    key_t shm_key = IPC_PRIVATE;
+    int ds_shm = shmget(shm_key, sizeof(int), IPC_CREAT | 0664);
 
- 	int * p = (int*) shmat(ds_shm, NULL, 0);
- 	if(p==(void*)-1) { 
- 		perror("errore shmget!");
- 		exit(1); 
- 	}
- 
- 	// parte di codice
- 	
- 	  shmctl(ds_shm, IPC_RMID, NULL);
+    if (ds_shm < 0) { 
+        perror("errore shmget!");
+        exit(1); 
+    }
 
-        return 0;
+    int *p = (int*) shmat(ds_shm, NULL, 0);
+    if (p == (void*)-1) { 
+        perror("errore shmat!");
+        exit(1); 
+    }
+
+    // parte di codice che utilizza la memoria condivisa
+
+    shmctl(ds_shm, IPC_RMID, NULL);
+
+    return 0;
 }
